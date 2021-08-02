37d918df4bed96d506fec4ce5e4cfb47c5eee052
Vladimir Pokutniy <vovchikp.1989@gmail.com>
2021-08-02 20:13:57 +0300

    wip

--- testapp/urls/urls.py
+++ testapp/urls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from testapp.views import index
-
+from django.contrib import admin
 
 urlpatterns = [
 
     path('', index, name='index'),
+    path('admin/', admin.site.urls),
 
 ]
